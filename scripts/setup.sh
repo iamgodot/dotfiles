f0e2ab36306376e84cebb1fd5cd25240cbb2c3f5
iamgodot <xugodot@gmail.com>
2021-09-29 22:00:43 +0800

    style: reformat setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/bash
 
 # Setup zsh
-if [ -f $HOME/.zshrc ]; then
-	mv $HOME/.zshrc /tmp/.zshrc.bk
-	echo "Backed zshrc to /tmp/.zshrc.bk"
+if [ -f $HOME/.zshrc ]
+then
+    mv $HOME/.zshrc /tmp/.zshrc.bk
+    echo "Backed up old zshrc to /tmp/.zshrc.bk"
 fi
 
 # Setup tmux
-if [ -f $HOME/.tmux.conf ]; then
-	mv $HOME/.tmux.conf /tmp/.tmux.conf.bk
-	echo "Backed tmux conf to /tmp/.tmux.conf.bk"
+if [ -f $HOME/.tmux.conf ]
+then
+    mv $HOME/.tmux.conf /tmp/.tmux.conf.bk
+    echo "Backed up old tmux conf to /tmp/.tmux.conf.bk"
 fi
 
 # Setup neovim
 mkdir -p $HOME/.config/nvim
-if [ -f $HOME/.config/nvim/init.vim ]; then
-	mv $HOME/.config/nvim/init.vim /tmp/init.vim.bk
-	echo "Backed init.vim to /tmp/init.vim.bk"
+if [ -f $HOME/.config/nvim/init.vim ]
+then
+    mv $HOME/.config/nvim/init.vim /tmp/init.vim.bk
+    echo "Backed up old init.vim to /tmp/init.vim.bk"
 fi
 
 ln -s $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc

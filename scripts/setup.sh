6fa9ca5b3669c5eab956bc059582561dd3e07d15
iamgodot <xugodot@gmail.com>
2021-09-29 21:59:59 +0800

    feat: set up git configs.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -22,3 +22,29 @@ fi
 ln -s $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc
 ln -s $HOME/.dotfiles/tmux/tmux.conf.symlink $HOME/.tmux.conf
 ln -s $HOME/.dotfiles/nvim/init.vim.symlink $HOME/.config/nvim/init.vim
+
+setup_gitconfig () {
+    if [ -f $HOME/.gitconfig ]
+    then
+        mv $HOME/.gitconfig /tmp/.gitconfig.bk
+        echo "Backed up old git config to /tmp/.gitconfig.bk"
+    fi
+    if [ -f $HOME/.gitconfig.local ]
+    then
+        mv $HOME/.gitconfig.local /tmp/.gitconfig.local.bk
+        echo "Backed up old git local config to /tmp/.gitconfig.local.bk"
+    fi
+        echo 'What is your github author name?'
+        read -e git_author_name
+        echo 'What is your github author email?'
+        read -e git_author_email
+
+        sed -e "s/AUTHORNAME/$git_author_name/g" -e "s/AUTHOREMAIL/$git_author_email/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
+        ln -s $HOME/.dotfiles/git/gitconfig.symlink $HOME/.gitconfig
+        ln -s $HOME/.dotfiles/git/gitconfig.local.symlink $HOME/.gitconfig.local
+        ln -s $HOME/.dotfiles/git/gitmessage.symlink $HOME/.gitmessage
+
+        echo 'Finished git config setup.'
+}
+
+setup_gitconfig

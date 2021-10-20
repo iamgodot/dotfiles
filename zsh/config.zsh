53ff49a88b73466f60644452535449165fdaa24a
iamgodot <xugodot@gmail.com>
2021-10-20 18:20:19 +0800

    fix: make functions autoload work

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -17,15 +17,8 @@ export EDITOR="$VISUAL"
 
 setopt histignorealldups
 
-# FIXME: autoload not working currently
 fpath=($HOME/.dotfiles/functions $fpath)
 autoload -U $HOME/.dotfiles/functions/*(:t)
-
-f() {
-  local files
-  IFS=$'\n' files=($(fzf --height 40% --query="$1" --multi --preview 'cat {}'))
-  [[ -n "$files" ]] && ${EDITOR:-nvim} "${files[@]}"
-}
 
 # Pew
 source $(pew shell_config)

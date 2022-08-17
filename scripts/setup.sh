fc2e0599925bd9cfadce3451b8a93c86b6081353
iamgodot <xugodot@gmail.com>
2022-08-17 16:24:21 +0800

    Upgrade setup script with better backup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,53 +1,24 @@
 #!/usr/bin/bash
 
-# Setup zsh
-if [ -f $HOME/.zshrc ]
-then
-    mv $HOME/.zshrc /tmp/.zshrc.bk
-    echo "Backed up old zshrc to /tmp/.zshrc.bk"
-fi
+echo 'Existing config files will be backed up using `ln -b`.'
 
-# Setup tmux
-if [ -f $HOME/.tmux.conf ]
-then
-    mv $HOME/.tmux.conf /tmp/.tmux.conf.bk
-    echo "Backed up old tmux conf to /tmp/.tmux.conf.bk"
-fi
-
-# Setup neovim
-mkdir -p $HOME/.config/nvim
-if [ -f $HOME/.config/nvim/init.vim ]
-then
-    mv $HOME/.config/nvim/init.vim /tmp/init.vim.bk
-    echo "Backed up old init.vim to /tmp/init.vim.bk"
-fi
-
-ln -s $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc
-ln -s $HOME/.dotfiles/tmux/tmux.conf.symlink $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/nvim/init.vim.symlink $HOME/.config/nvim/init.vim
+echo 'Setting up config files for zsh & tmux & neovim now.'
+ln -s $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc -b
+ln -s $HOME/.dotfiles/tmux/tmux.conf.symlink $HOME/.tmux.conf -b
+ln -s $HOME/.dotfiles/nvim/init.vim.symlink $HOME/.config/nvim/init.vim -b
+echo 'Finished setup for the suite.'
 
 setup_gitconfig () {
-    if [ -f $HOME/.gitconfig ]
-    then
-        mv $HOME/.gitconfig /tmp/.gitconfig.bk
-        echo "Backed up old git config to /tmp/.gitconfig.bk"
-    fi
-    if [ -f $HOME/.gitconfig.local ]
-    then
-        mv $HOME/.gitconfig.local /tmp/.gitconfig.local.bk
-        echo "Backed up old git local config to /tmp/.gitconfig.local.bk"
-    fi
-        echo 'What is your github author name?'
-        read -e git_author_name
-        echo 'What is your github author email?'
-        read -e git_author_email
-
-        sed -e "s/AUTHORNAME/$git_author_name/g" -e "s/AUTHOREMAIL/$git_author_email/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
-        ln -s $HOME/.dotfiles/git/gitconfig.symlink $HOME/.gitconfig
-        ln -s $HOME/.dotfiles/git/gitconfig.local.symlink $HOME/.gitconfig.local
-        ln -s $HOME/.dotfiles/git/gitmessage.symlink $HOME/.gitmessage
-
-        echo 'Finished git config setup.'
+    echo 'Setting up git config files now.'
+    echo 'What is your github author name?'
+    read -e git_author_name
+    echo 'What is your github author email?'
+    read -e git_author_email
+    sed -e "s/AUTHORNAME/$git_author_name/g" -e "s/AUTHOREMAIL/$git_author_email/g" $HOME/.dotfiles/git/gitconfig.local.symlink.example > $HOME/.dotfiles/git/gitconfig.local.symlink
+    ln -s $HOME/.dotfiles/git/gitconfig.symlink $HOME/.gitconfig -b
+    ln -s $HOME/.dotfiles/git/gitconfig.local.symlink $HOME/.gitconfig.local -b
+    ln -s $HOME/.dotfiles/git/gitmessage.symlink $HOME/.gitmessage -b
+    echo 'Finished git config setup.'
 }
 
 setup_gitconfig

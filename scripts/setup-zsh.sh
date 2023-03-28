2e473f922e829de76529fe04aa22a312c6b9bad4
iamgodot <xugodot@gmail.com>
2023-03-28 12:47:32 +0800

    Install via git

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -8,8 +8,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 # Install p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # Nerd fonts
-brew tap homebrew/cask-fonts
-brew install font-hack-nerd-font
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git $HOME/nerd-fonts
+$HOME/nerd-fonts/install.sh
 
 # Fzf
-$(brew --prefix)/opt/fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install

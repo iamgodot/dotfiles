9e29fb1b3404f3f61beaf62b1f709890b39ef10b
iamgodot <xugodot@gmail.com>
2023-04-04 18:27:41 +0800

    Remove unused lsp servers

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -45,11 +45,7 @@ return {
         --  Add any additional override configuration in the following tables. They will be passed to
         --  the `settings` field of the server config. You must look up that documentation yourself.
         local servers = {
-            -- clangd = {},
-            -- gopls = {},
             pyright = {},
-            -- rust_analyzer = {},
-            -- tsserver = {},
         }
 
         -- nvim-cmp supports additional completion capabilities, so broadcast that to servers

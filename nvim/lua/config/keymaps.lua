b543e8eccc74a26f6d356c48d6bcf4bb458c024f
iamgodot <xugodot@gmail.com>
2023-04-05 00:20:01 +0800

    Use trouble instead of this

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,9 +17,3 @@ vim.keymap.set("n", "tp", ":tabp<CR>")
 vim.keymap.set("n", "tt", ":tabnew")
 vim.keymap.set("n", "tc", ":tabclose")
 vim.keymap.set("n", "ts", ":tab split<CR>")
-
--- Diagnostic
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })

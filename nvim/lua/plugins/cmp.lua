53dfd8a822b2696fc2cf8b4a0c6d941e3bf0fd19
iamgodot <xugodot@gmail.com>
2023-04-05 16:31:53 +0800

    Add snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,12 +1,20 @@
 return {
 	-- Autocompletion
 	"hrsh7th/nvim-cmp",
-	dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-path" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-path",
+		"rafamadriz/friendly-snippets",
+	},
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 
 		luasnip.config.setup({})
+		-- Load snippets
+		require("luasnip/loaders/from_vscode").lazy_load()
 
 		cmp.setup({
 			snippet = {

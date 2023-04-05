ce793393dae2d77310bb653221f3515be21ecaee
iamgodot <xugodot@gmail.com>
2023-04-05 17:05:40 +0800

    Add cool keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,3 +17,17 @@ vim.keymap.set("n", "tp", ":tabp<CR>")
 vim.keymap.set("n", "tt", ":tabnew")
 vim.keymap.set("n", "tc", ":tabclose")
 vim.keymap.set("n", "ts", ":tab split<CR>")
+
+-- Stay in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Move block up&down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Smarter copy&paste
+vim.keymap.set("x", "<leader>x", '"_dP')
+vim.keymap.set("v", "<leader>y", '"+y')

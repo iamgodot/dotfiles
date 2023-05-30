fd5a82cad1314c365cd9250f21f04c79615a8db1
iamgodot <xugodot@gmail.com>
2023-05-30 17:22:47 +0800

    Use nvim-tmux-navigation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,12 +5,6 @@
 vim.keymap.set("i", "<C-c>", "<ESC>")
 vim.keymap.set("n", "<C-p>", ":noh<CR>")
 
--- Move between panes
-vim.keymap.set("n", "<C-j>", ":1wincmd j<CR>", { noremap = true })
-vim.keymap.set("n", "<C-k>", ":1wincmd k<CR>", { noremap = true })
-vim.keymap.set("n", "<C-h>", ":1wincmd h<CR>", { noremap = true })
-vim.keymap.set("n", "<C-l>", ":1wincmd l<CR>", { noremap = true })
-
 -- Tabs
 vim.keymap.set("n", "tn", ":tabn<CR>")
 vim.keymap.set("n", "tp", ":tabp<CR>")

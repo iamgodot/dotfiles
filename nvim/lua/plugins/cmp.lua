f05c8cfbb81bfaee38c3737aa45b7a7ee4266d1d
iamgodot <xugodot@gmail.com>
2023-09-17 20:21:45 -0700

    Add buffer support, and refine with keybinding&styling

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -5,16 +5,20 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
         "rafamadriz/friendly-snippets",
     },
     config = function()
+        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+        require("luasnip/loaders/from_vscode").lazy_load()
+
         local cmp = require("cmp")
         local luasnip = require("luasnip")
 
-        luasnip.config.setup({})
-        -- Load snippets
-        require("luasnip/loaders/from_vscode").lazy_load()
+        local select_opts = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
             snippet = {
@@ -23,13 +27,30 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete({}),
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                -- Overriden by my input source switch shortcut
+                -- ["<C-Space>"] = cmp.mapping.complete({}),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
+                    -- behavior = cmp.ConfirmBehavior.Replace,
+                    select = false,
                 }),
+                ["<C-f>"] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<C-b>"] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
@@ -50,9 +71,29 @@ return {
                 end, { "i", "s" }),
             }),
             sources = {
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
+                { name = "nvim_lsp_signature_help" },
                 { name = "path" },
+                { name = "nvim_lsp", keyword_length = 1 },
+                { name = "buffer", keyword_length = 3 },
+                { name = "luasnip", keyword_length = 2 },
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            formatting = {
+                fields = { "menu", "abbr", "kind" },
+                format = function(entry, item)
+                    local menu_icon = {
+                        nvim_lsp = "lsp",
+                        luasnip = "snip",
+                        buffer = "buf",
+                        path = "path",
+                    }
+
+                    item.menu = menu_icon[entry.source.name]
+                    return item
+                end,
             },
         })
     end,

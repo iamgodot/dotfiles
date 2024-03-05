40e2386db39e7642a937c57b1aaa7e8c4bcfb742
iamgodot <xugodot@gmail.com>
2024-03-05 10:19:51 -0800

    Replace airline and tmuxline with lualine

--- nvim/lua/plugins/tmuxline.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "edkolev/tmuxline.vim",
-    config = function()
-        vim.g["airline#extensions#tmuxline#enabled"] = 1
-        vim.g.tmuxline_powerline_separators = 0
-    end,
-}

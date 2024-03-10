9d6e1f4c8d1a1d2546ed69890e4b41cedd646758
iamgodot <xugodot@gmail.com>
2024-03-09 22:59:53 -0800

    Customize file path and symbols

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,7 +4,19 @@ return {
     config = function()
         local lualine = require("lualine")
         lualine.setup({
-            options = { theme = "solarized_dark", section_separators = "", component_separators = "" },
+            options = {
+                theme = "solarized_dark",
+                section_separators = "",
+                component_separators = "",
+                file_status = true,
+                path = 1,
+                symbols = {
+                    modified = "[Modified]",
+                    readonly = "[Read-only]",
+                    unnamed = "[No Name]",
+                    newfile = "[New]",
+                },
+            },
         })
     end,
 }

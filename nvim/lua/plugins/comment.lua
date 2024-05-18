0c3e9e0861f0237814cfc65a4ed7c01e0af5f51f
iamgodot <xugodot@gmail.com>
2024-05-17 23:06:58 -0700

    Switch to Comment.nvim;also add support for jsx/tsx files

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,9 +1,27 @@
 return {
-    "terrortylor/nvim-comment",
+    "numToStr/Comment.nvim",
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
     config = function()
-        require("nvim_comment").setup({
-            line_mapping = "<leader>cc",
-            operator_mapping = "<leader>c",
+        require("ts_context_commentstring").setup({
+            enable_autocmd = false,
+        })
+        require("Comment").setup({
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+            toggler = {
+                ---Line-comment toggle keymap
+                line = "<leader>cc",
+                ---Block-comment toggle keymap
+                block = "<leader>bc",
+            },
+            ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+            opleader = {
+                ---Line-comment keymap
+                line = "<leader>c",
+                ---Block-comment keymap
+                block = "<leader>b",
+            },
         })
     end,
 }

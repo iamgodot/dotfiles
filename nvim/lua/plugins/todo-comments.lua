3344f6e9b93e81ebbf9c02fe0e8a8943fc712cbf
iamgodot <xugodot@gmail.com>
2024-05-22 11:22:05 -0700

    Change loading key

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -2,7 +2,7 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-        { "<leader>t", "<cmd>TodoTelescope<cr>", desc = "Todo-comments" },
+        { "<leader>td", "<cmd>TodoTelescope<cr>", desc = "Todo-comments" },
     },
     config = true,
 }

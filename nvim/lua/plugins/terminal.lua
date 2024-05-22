8da4cceaf3e98f31d4079bfca75ab852012c3d71
iamgodot <xugodot@gmail.com>
2024-05-22 11:27:01 -0700

    Add toggleterm

---
+++ nvim/lua/plugins/terminal.lua
@@ -0,0 +1,8 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+        { "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Open terminal" },
+    },
+    config = true,
+}

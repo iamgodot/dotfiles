16d8b001d7bc77f6238386483155f3045c052642
iamgodot <xugodot@gmail.com>
2024-05-22 15:53:21 -0700

    Add chatgpt plugin

---
+++ nvim/lua/plugins/gpt.lua
@@ -0,0 +1,15 @@
+return {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+        popup_input = {
+            submit = "<CR>",
+        },
+    },
+}

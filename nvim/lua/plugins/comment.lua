1243e1332f26084048310a94f2573625c740335d
iamgodot <xugodot@gmail.com>
2024-05-23 21:48:50 -0700

    Replace with ts-comments

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,25 +1,6 @@
 return {
-    "numToStr/Comment.nvim",
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
-        require("ts_context_commentstring").setup({
-            enable_autocmd = false,
-        })
-        require("Comment").setup({
-            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-            toggler = {
-                ---Line-comment toggle keymap
-                line = "<leader>cc",
-                ---Block-comment toggle keymap
-                block = "<leader>bc",
-            },
-            ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-            opleader = {
-                ---Line-comment keymap
-                line = "<leader>c",
-            },
-        })
-    end,
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
 }

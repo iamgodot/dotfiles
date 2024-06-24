1d86aa62efb4d57963c96666870f7e534beb7092
iamgodot <xugodot@gmail.com>
2024-06-24 12:59:47 -0700

    Update fzf config

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,3 +1,4 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
 
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude .pyc'

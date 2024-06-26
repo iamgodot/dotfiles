5f2f44eac7659c2b0ffce4b9d2777fe59822b500
iamgodot <xugodot@gmail.com>
2024-06-26 15:20:39 -0700

    Use hyprlock for screen lock

--- arch/hypr/scripts/sleep.sh
+++ arch/hypr/scripts/sleep.sh
@@ -1,3 +1,3 @@
-swayidle -w timeout 300 'swaylock -f -c 000000' \
+swayidle -w timeout 300 'hyprlock' \
             timeout 600 'systemctl suspend' &
             # before-sleep 'swaylock -f -c 000000' &

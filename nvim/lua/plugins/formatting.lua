640d52f1370b457196607669973dcd7d14890603
iamgodot <xugodot@gmail.com>
2024-08-01 17:36:10 -0700

    Replace null-ls with conform and nvim-lint

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,45 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        -- TODO: let Mason handle installation of formatters
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "black" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                ["_"] = { "trim_newlines", "trim_whitespace" },
+            },
+            formatters = {
+                stylua = {
+                    prepend_args = { "--indent-type", "Spaces" },
+                },
+                black = {
+                    condition = function(self, ctx)
+                        return string.match(ctx.filename, "source") == nil
+                    end,
+                },
+                prettier = {
+                    condition = function(self, ctx)
+                        return string.match(ctx.filename, "source") == nil
+                    end,
+                },
+            },
+        })
+        conform.setup({
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "never",
+            },
+        })
+    end,
+}

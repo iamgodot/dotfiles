a8d225b593d86813c11682e99e626569895138c9
iamgodot <xugodot@gmail.com>
2025-01-06 16:47:47 -0800

    Refine lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,86 +6,36 @@ return {
     },
     config = function()
         local lsp = require("lspconfig")
-        local opts = { noremap = true, silent = true }
-        local on_attach = function(bufnr)
-            opts.buffer = bufnr
-
-            opts.desc = "Rename"
-            vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-
-            opts.desc = "Go to definition"
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
-            opts.desc = "Go to references"
-            vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
-
-            opts.desc = "Document symbols"
-            vim.keymap.set("n", "<leader>j", require("telescope.builtin").lsp_document_symbols, opts)
-
-            opts.desc = "Workspace symbols"
-            vim.keymap.set("n", "<leader>jj", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
-
-            opts.desc = "Hover Documentation"
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-            opts.desc = "Go to declaration"
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-            opts.desc = "Code action"
-            vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
-        end
-        local capabilities_py = vim.lsp.protocol.make_client_capabilities()
-        capabilities_py.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-        -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        lsp.pyright.setup({
-            on_attach = on_attach,
-            capabilities = capabilities_py,
-        })
         lsp.ruff.setup({
-            on_attach = on_attach,
             init_options = {
                 settings = {
-                    args = {
-                        "--select",
-                        "B,F,E,W,I",
+                    -- Ruff language server settings go here
+                },
+            },
+        })
+        lsp.pyright.setup({
+            settings = {
+                pyright = {
+                    -- Using Ruff's import organizer
+                    disableOrganizeImports = true,
+                },
+                python = {
+                    analysis = {
+                        -- Ignore all files for analysis to exclusively use Ruff for linting
+                        ignore = { "*" },
                     },
                 },
             },
         })
-        lsp.ts_ls.setup({
-            on_attach = on_attach,
-            init_options = {
-                preferences = {
-                    disableSuggestions = true,
-                },
-            },
-        })
-        lsp.lua_ls.setup({
-            on_attach = on_attach,
-            init_options = {
-                workspace = {
-                    checkThirdParty = true,
-                },
-            },
-        })
-        lsp.tailwindcss.setup({})
+        lsp.vtsls.setup({})
         lsp.emmet_language_server.setup({})
         lsp.prismals.setup({})
         lsp.clangd.setup({})
-        -- Global mappings.
-        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-        vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-
         -- Use LspAttach autocommand to only map the following keys
         -- after the language server attaches to the current buffer
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
-                -- Enable completion triggered by <c-x><c-o>
-                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
                 local opts = { buffer = ev.buf }
@@ -103,15 +53,6 @@ return {
 
                 opts.desc = "Workspace symbols"
                 vim.keymap.set("n", "<leader>jj", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
-
-                opts.desc = "Hover Documentation"
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-                opts.desc = "Go to declaration"
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-                opts.desc = "Code action"
-                vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
             end,
         })
     end,

c9633d9b9945e4dc6fdd4496941c28598e01b2c6
iamgodot <xugodot@gmail.com>
2025-03-09 15:23:50 -0700

    Fix telescope keymap not working

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,31 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.8",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         "debugloop/telescope-undo.nvim",
     },
     keys = {
-        { "<leader>u", "<cmd>Telescope undo<cr>", desc = "[S]earch [U]ndos" },
+        { "<leader>u", "<cmd>Telescope undo<cr>", desc = "Telescope undos" },
+        {
+            "<leader>p",
+            "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+            desc = "Telescope find files",
+        },
+        { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+        {
+            "<leader>/",
+            "<cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({winblend=10,previewer=false}))<cr>",
+            desc = "Telescope fuzzy find in current buffer",
+        },
+        { "<leader>h", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+        { "<leader>w", "<cmd>Telescope grep_string<cr>", desc = "Telescope grep string" },
+        { "<leader>f", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+        { "<leader>k", "<cmd>Telescope keymaps<cr>", desc = "Telescope keymaps" },
+        { "<leader>cm", "<cmd>Telescope commands<cr>", desc = "Telescope commands" },
+        { "<leader>cs", "<cmd>Telescope colorscheme<cr>", desc = "Telescope colorschemes" },
+        { "<leader>v", "<cmd>Telescope vim_options<cr>", desc = "Telescope vim options" },
     },
     config = function()
         local telescope = require("telescope")
@@ -59,28 +77,5 @@ return {
 
         require("telescope").load_extension("fzf")
         require("telescope").load_extension("undo")
-
-        -- See `:help telescope.builtin`
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "[ ] Find existing buffers" })
-        vim.keymap.set("n", "<leader>/", function()
-            builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                winblend = 10,
-                previewer = false,
-            }))
-        end, { desc = "[/] Fuzzily search in current buffer" })
-        vim.keymap.set(
-            "n",
-            "<leader>p",
-            "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
-            { desc = "[S]earch [F]iles" }
-        )
-        vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>w", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-        vim.keymap.set("n", "<leader>k", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-        vim.keymap.set("n", "<leader>cm", builtin.commands, { desc = "[S]earch [C]ommands" })
-        vim.keymap.set("n", "<leader>cs", builtin.colorscheme, { desc = "[S]earch [C]olorschemes" })
-        vim.keymap.set("n", "<leader>v", builtin.vim_options, { desc = "[S]earch [V]im options" })
     end,
 }

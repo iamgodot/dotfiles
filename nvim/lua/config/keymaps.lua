2f1a8ecaecf5228462b848e86b9e8faedd989b7f
iamgodot <xugodot@gmail.com>
2025-08-09 11:27:46 -0700

    Make neovim return to last edit position when opening files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -38,3 +38,15 @@ vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window s
 
 -- Pull out Lazy
 vim.keymap.set("n", "<leader>la", ":Lazy<CR>")
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})

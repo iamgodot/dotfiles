a6bd34483f0193a4c42e28ed4dc8bd5eb603021b
iamgodot <xugodot@gmail.com>
2025-08-14 23:25:16 -0700

    Refine neovim dashboard ui

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -29,7 +29,7 @@ return {
                         icon = " ",
                         key = "c",
                         desc = "Config",
-                        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+                        action = ":lua Snacks.dashboard.pick('files', {cwd = '~/.config/nvim/lua'})",
                     },
                     { icon = " ", key = "s", desc = "Restore Session", section = "session" },
                     {
@@ -45,8 +45,8 @@ return {
             sections = {
                 { section = "header" },
                 { section = "keys", gap = 1, padding = 2 },
-                { icon = " ", title = "Recent Files", section = "recent_files", indent = 0, padding = { 2, 1 } },
-                { icon = " ", title = "Projects", section = "projects", indent = 0, padding = { 2, 1 } },
+                -- { icon = " ", title = "Recent Files", section = "recent_files", indent = 0, padding = { 2, 1 } },
+                -- { icon = " ", title = "Projects", section = "projects", indent = 0, padding = { 2, 1 } },
                 { section = "startup" },
             },
         },

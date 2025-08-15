fc6345764933ec2bff4acf9807002e80d6cdfd2a
iamgodot <xugodot@gmail.com>
2025-08-14 23:25:55 -0700

    Update wezterm ui config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,14 +2,14 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
-config.font_size = 16
+config.font_size = 15
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 config.color_scheme = "Catppuccin Macchiato"
 
--- config.window_background_opacity = 0.8
+-- config.window_background_opacity = 0.7
 -- config.macos_window_background_blur = 10
 
 return config

a122d5a9b26faab8be2a296b52291ef1517b787d
iamgodot <xugodot@gmail.com>
2025-11-12 11:20:26 -0800

    Toggle on search and char jump labels

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,7 +1,7 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = { modes = { search = { enabled = true }, char = { jump_labels = true } } },
     -- stylua: ignore
     keys = {
         { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },

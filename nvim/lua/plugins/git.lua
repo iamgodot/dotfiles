121c582fa5e6477567bcf565379631645a1d98cc
iamgodot <xugodot@gmail.com>
2025-11-11 18:29:43 -0800

    Replace telescope with snacks and add keymap to close diffview

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,11 +2,13 @@ return {
     "NeogitOrg/neogit",
     keys = {
         { "gs", "<cmd>Neogit<cr>", desc = "Neogit" },
+        { "df", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
     },
     dependencies = {
         "nvim-lua/plenary.nvim", -- required
         "sindrets/diffview.nvim", -- optional - Diff integration
-        "nvim-telescope/telescope.nvim", -- optional
+        "folke/snacks.nvim", -- optional
     },
     config = true,
+    opts = { view = { merge_tool = { layout = "diff3_mixed" } } },
 }

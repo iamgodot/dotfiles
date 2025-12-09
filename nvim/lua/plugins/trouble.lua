d6cd3520e8a441c3b745c807230777d14f335b5c
iamgodot <xugodot@gmail.com>
2025-12-08 23:47:18 -0800

    Add plugin for symbol outline

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -13,25 +13,5 @@ return {
             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
             desc = "Buffer Diagnostics (Trouble)",
         },
-        {
-            "<leader>xl",
-            "<cmd>Trouble symbols toggle focus=true<cr>",
-            desc = "Symbols (Trouble)",
-        },
-        {
-            "<leader>x;",
-            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "LSP Definitions / references / ... (Trouble)",
-        },
-        {
-            "<leader>xL",
-            "<cmd>Trouble loclist toggle<cr>",
-            desc = "Location List (Trouble)",
-        },
-        {
-            "<leader>xQ",
-            "<cmd>Trouble qflist toggle<cr>",
-            desc = "Quickfix List (Trouble)",
-        },
     },
 }
